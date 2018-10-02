82cb7f2081e0ba686fa78fead6d395b45ab91782
Tobias Wiedemann <webdev02@fundconnect.com>
2018-10-02 11:06:36 +0200

    Added snippet for insertion of 3 test users

--- stackoverflow_test_queries.sql
+++ stackoverflow_test_queries.sql
@@ -1,10 +1,19 @@
+-- Insert some test users
+insert into users
+    (display_name, creation_date, email, "password", deactivation_date)
+values
+    ('Begoña', now()::timestamp(0), 'begona@test.local', '123', null),
+    ('Tobias', now()::timestamp(0), 'tobias@test.local', '123', null),
+    ('Tomas', now()::timestamp(0), 'tomas@test.local', '123', null);
+
 -- Filter test queries
 select * from filter_by_words(
+    null /*user_id*/,
     'testing unit',
     array['.net', '.htaccess', 'ajax', 'javascript', 'c#', 'sql']::text[]
 );
 
-select * from filter_by_words('testing unit');
+select * from filter_by_words(null /*user_id*/, 'testing unit');
 
 select * from filter_by_tags(
     array['.net', '.htaccess', 'ajax', 'javascript', 'c#', 'sql']::text[]
