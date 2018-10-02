3b778444e49efeef2018912fa747fea3da5ad53e
Tobias Wiedemann <webdev02@fundconnect.com>
2018-10-02 11:06:17 +0200

    Added search history insertion to "filter_by_words" function

--- stackoverflow_functions.sql
+++ stackoverflow_functions.sql
@@ -1,13 +1,5 @@
--- View which include post tags
-create view posts_with_tags as
-select posts.*, array_agg(post_tags.name) tags
-from posts
-join post_tags
-on posts.id = post_tags.post_id
-group by posts.id;
-
 -- Function for filtering by post tags
-create function filter_by_tags(_tags text[])
+create or replace function filter_by_tags(_tags text[])
 returns setof posts_with_tags as $$
     begin
         return query
@@ -21,27 +13,28 @@ returns setof posts_with_tags as $$
 $$ language plpgsql;
 
 -- Function for filtering by words occurring in the post title and tags
-create function filter_by_words(_user_id integer, _search text = null, _tags text[] = null)
+create or replace function filter_by_words(_user_id integer = null, _search text = null, _tags text[] = null)
 returns setof posts_with_tags as $$
     declare
-        _search_query tsquery,
+        _query tsquery;
         _flag boolean;
     begin
-        _search_query := plainto_tsquery('english', _search);
+        _query := plainto_tsquery('english', _search);
         _flag := (_search = '') is not false;
 
-        if _flag then
-
+        if not _flag and _user_id is not null then
+            perform add_to_search_history(_user_id, _search);
+            raise notice 'should: insert into search_history values(user_id, search_string)';
         end if;
 
         if _tags is not null then
             return query
                 select * from filter_by_tags(_tags)
-                where (_flag or title_tokens @@ _search_query);
+                where (_flag or title_tokens @@ _query);
         else
             return query
                 select * from posts_with_tags
-                where (_flag or title_tokens @@ _search_query);
+                where (_flag or title_tokens @@ _query);
         end if;
     end
 $$ language plpgsql;
