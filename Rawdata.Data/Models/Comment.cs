d0b388e0705636052461aacce09366a68338b335
TomasNemecek <tomas@tomasnemecek.com>
2018-10-24 23:32:20 +0200

    #3 - Implemented Author, Comment and Post Domain Models

--- Rawdata.Data/Models/Comment.cs
+++ Rawdata.Data/Models/Comment.cs
@@ -1,7 +1,18 @@
-﻿namespace Rawdata.Data.Models
+﻿using System;
+
+namespace Rawdata.Data.Models
 {
     public class Comment
     {
-        
+        public int Id { get; set; }
+        public int Score { get; set; }
+        public string Text { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+
+        public int PostId { get; set; }
+        public Post Post { get; set; }
     }
 }
--- Rawdata.Data/Models/Post.cs
+++ Rawdata.Data/Models/Post.cs
@@ -1,7 +1,29 @@
-﻿namespace Rawdata.Data.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rawdata.Data.Models
 {
     public class Post
     {
         //TODO Add nullable property for array of post tags 
+        public int Id { get; set; }
+        public int TypeId { get; set; }
+        public int Score { get; set; }
+        public string Body { get; set; }
+        public DateTime? CreationDate { get; set; }
+        public DateTime? ClosedDate { get; set; }
+        public string Title { get; set; }
+
+        public int? ParentId { get; set; }
+        public Post Parent { get; set; }
+        public ICollection<Post> ChildrenPosts { get; set; }
+
+        public int? AcceptedAnswerId { get; set; }
+        public Post AcceptedAnswer { get; set; }
+
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+
+        public ICollection<Comment> Comments { get; set; }
     }
 }
