37328c0c2ca700acfb48f4401497daaa84c5ca6c
Tobias <cchawps@gmail.com>
2018-11-01 18:14:38 +0100

    #10 shortened startup configurations

--- Rawdata.Service/Startup.cs
+++ Rawdata.Service/Startup.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using Rawdata.Data;
 using Rawdata.Data.Models;
@@ -32,19 +25,17 @@ namespace Rawdata.Service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services
-                .AddMvc()
-                .AddJsonOptions(options => {
-                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-                    options.SerializerSettings.Formatting = Formatting.Indented;
-                })
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            // services.AddScoped<DataContext>();
 
-            services.AddScoped<DataContext>();
+            services.AddDbContext<DataContext>();
             services.AddScoped<ICommentRepository, CommentRepository>();
             services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IMapper>(_ => CreateMapper());
 
-            services.AddSingleton<IMapper>(serviceProvider => CreateMapperInstance());
+            services
+                .AddMvc()
+                .AddJsonOptions(_ => CreateSerializerSettings())
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -63,14 +54,13 @@ namespace Rawdata.Service
             app.UseMvc();
         }
 
-        private IMapper CreateMapperInstance()
+        private JsonSerializerSettings CreateSerializerSettings() => new JsonSerializerSettings
         {
-            var mapperConfiguration = new MapperConfiguration(cfg =>
-            {
-                cfg.CreateMap<User, UserDto>();
-            });
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            Formatting = Formatting.Indented
+        };
 
-            return mapperConfiguration.CreateMapper();
-        }
+        private IMapper CreateMapper() =>
+            new MapperConfiguration(cfg => { cfg.CreateMap<User, UserDto>(); }).CreateMapper();
     }
 }
