4dec1a31b20d2451308198ef3f966e4da6b78e03
Begoña Sustatxa <bego.sustatxa1.d@gmail.com>
2018-12-11 12:39:14 +0100

    GetExactMatchAsync changes

--- Rawdata.Data/Services/Interfaces/ISearchResultService.cs
+++ Rawdata.Data/Services/Interfaces/ISearchResultService.cs
@@ -6,7 +6,7 @@ namespace Rawdata.Data.Services.Interfaces
 {
     public interface ISearchResultService
     {
-        IQueryable<SearchResult> GetExactMatch(int page, int size, params string[] words);
+        Task<PaginatedResult<SearchResult>> GetExactMatchAsync(int page, int size, params string[] words);
         IQueryable<SearchResult> GetBestMatch(int page, int size, params string[] words);
         IQueryable<SearchResult> GetRankedWeightedMatch(int page, int size, params string[] words);
         IQueryable<WeightedKeyword> GetWeightedKeywords(int size, string word);
