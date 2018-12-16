958531c5122869e43ee01fdd14a6052c3a3218a3
Tobias <cchawps@gmail.com>
2018-12-16 20:11:19 +0100

    marking of texts

--- frontend/src/bindings/highlightText.js
+++ frontend/src/bindings/highlightText.js
@@ -1,5 +1,3 @@
-import { unwrap } from 'knockout'
-
 const getSentences = text => {
     const element = document.createElement('div')
     element.innerHTML = text
@@ -16,22 +14,27 @@ const getSentences = text => {
         .flatMap(strings => [...strings])
 }
 
-const getRelevantSentences = (texts, toMark, joinBy) => {
+const getRelevantSentences = (texts, toMark, joinBy, length = 400) => {
     const regex = new RegExp(`\\b(${toMark.join('|')})\\b`, 'gi')
 
     return texts
         .filter(string => regex.test(string))
         .join(joinBy)
         .replace(/[<>]/g, i => '&#' + i.charCodeAt(0) + ';')
+        .substring(0, length)
         .replace(regex, '<mark>$&</mark>')
 }
 
-const stripHtmlAndMark = (text, toMark) => {
+const escapeHtmlAndMark = (text, toMark) => {
     const regex = new RegExp(`\\b(${toMark.join('|')})\\b`, 'gi')
 
     return text
         .replace(/[<>]/g, i => '&#' + i.charCodeAt(0) + ';')
         .replace(regex, '<mark>$&</mark>')
+}
+
+const escapeHtml = text => {
+    return text.replace(/[<>]/g, i => '&#' + i.charCodeAt(0) + ';')
 }
 
 const getPlainExcerpt = (text, length = 400) => {
@@ -43,6 +46,7 @@ const getPlainExcerpt = (text, length = 400) => {
         .filter(tag => /(p|code)/i.test(tag.tagName))
         .map(tag => tag.textContent)
         .join(' ')
+        .replace(/[<>]/g, i => '&#' + i.charCodeAt(0) + ';')
         .substring(0, length)
 
 }
@@ -50,7 +54,7 @@ const getPlainExcerpt = (text, length = 400) => {
 const getMarkedExcerpt = (text, toMark, joinBy = ' ... ') =>
     getRelevantSentences(getSentences(text), toMark, joinBy)
 
-export { getPlainExcerpt, getMarkedExcerpt, stripHtmlAndMark }
+export { getPlainExcerpt, getMarkedExcerpt, escapeHtmlAndMark, escapeHtml }
 
 // export default {
 //     init(element, valueAccessor, allBindings) {
